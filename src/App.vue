263d35eccff522de85c134970d6131d36f798820
liftcat <liftcat@163.com>
2022-09-10 18:48:38 +0800

    初始路由切换

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="app">App.vue</div>
+  <div id="app">
+    <router-view />
+
+    <van-tabbar route active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item replace to="/home" icon="home-o">主页</van-tabbar-item>
+      <van-tabbar-item replace to="/user" icon="user-circle-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'App'
+}
 </script>
 
 <style lang="less" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// view
+import HomeView from '@/views/Home/HomeView.vue'
+import UserView from '@/views/User/UserView.vue'
+
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  { path: '/', redirect: '/home' },
+  { path: '/home', component: HomeView },
+  { path: '/user', component: UserView }
+]
 
 const router = new VueRouter({
   routes
