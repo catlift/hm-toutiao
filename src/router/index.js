db892cf88960faeec799b0d2e4e864ae6d647f1c
liftcat <liftcat@163.com>
2022-09-10 19:26:24 +0800

    设置了到user页面的全局前置守卫

--- src/router/index.js
+++ src/router/index.js
@@ -4,17 +4,32 @@ import VueRouter from 'vue-router'
 // view
 import HomeView from '@/views/Home/HomeView.vue'
 import UserView from '@/views/User/UserView.vue'
+import LoginView from '@/views/Login/LoginView.vue'
+
+// pathArr ，拦截规则
+import pathArr from '@/router/pathArr.js'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/home' },
   { path: '/home', component: HomeView },
-  { path: '/user', component: UserView }
+  { path: '/user', component: UserView },
+  { path: '/login', component: LoginView }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  if (pathArr.includes(to.path)) {
+    // 判断是否登录，已登录跳转到 /user，未登录跳转到 /login
+    console.log(to.path)
+  } else {
+    next()
+  }
 })
 
 export default router
