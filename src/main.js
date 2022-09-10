ab10a781e859742fd887ce7d620119880ec0d231
liftcat <liftcat@163.com>
2022-09-10 17:46:10 +0800

    创建了项目，并安装导入了 vant2 组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// vant2
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
